12a99676c57d818d6d2470fa9bb6f898b96b4785
Yicheng Tang <105127382+tangycc@users.noreply.github.com>
2025-04-03 18:29:38 -0400

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,14 @@
+var video;
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+
+});
+
+// play video button
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+});
+
+
+
